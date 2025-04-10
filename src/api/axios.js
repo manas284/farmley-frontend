0b382c0197e64d49585a5397bbcef6cfbc3e6700
manas284 <139740351+manas284@users.noreply.github.com>
2025-04-10 08:09:39 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://farm1-3w3d.onrender.com/', // Replace with your backend URL
+  baseURL: 'https://farm1-3w3d.onrender.com/api', // Replace with your backend URL
 });
 
 export default axiosInstance;
